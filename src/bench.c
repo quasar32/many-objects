b5464850e149cf14d5594cb3ff30e56d972fd726
quasar32 <fontejam@tamu.edu>
2024-12-27 23:22:34 -0600

    fixed timing

--- src/bench.c
+++ src/bench.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <time.h>
 #include "sim.h"
 
+#define IS_LONG(v) _Generic((v), long: 1, default: 0)
+
+static long get_time(void) {
+    struct timespec t;
+    if (clock_gettime(CLOCK_THREAD_CPUTIME_ID, &t) < 0) {
+        perror("clock_gettime");
+        exit(EXIT_FAILURE);
+    }
+    static_assert(IS_LONG(t.tv_sec));
+    static_assert(IS_LONG(t.tv_nsec));
+    return t.tv_sec * 1000000000 + t.tv_nsec;
+}
+
 int main(int argc, char **argv) {
     init_sim();
-    clock_t begin = clock();
+    long t0 = get_time();
     for (int t = 0; t < N_FRAMES; t++) {
         step_sim();
     }
-    clock_t end = clock();
-    clock_t ms = (end - begin) / 1000;
-    fprintf(stderr, "%ld ms\n", ms); 
+    long t1 = get_time();
+    long dt = (t1 - t0) / 1000000; 
+    fprintf(stderr, "%ld ms\n", dt); 
     return 0;
 }
